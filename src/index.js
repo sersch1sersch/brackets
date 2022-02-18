f0b2c86b39d9fdc005a53b0c4c0ede02ba7a2916
sersch1sersch <sersch11031984@gmail.com>
2022-02-18 17:18:49 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let xArray = [];
+  bracketsConfig.forEach(items => {
+    xArray.push(items.join(''))
+  });
+  
+  let y = 0;
+  while( y < xArray.length){
+    xArray.forEach(items => {
+      while(str.indexOf(items) !== -1){
+          str = str.replace(items, '');
+          y = 0;    
+      }
+      y++;
+    })
+  }
+  return (str.length !== 0) ? false : true;
 }
